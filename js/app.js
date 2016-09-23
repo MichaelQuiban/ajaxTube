55d283156ec45d4a13be4699603040b3876f7e94
Michael Q <MichaelQuiban@outlook.com>
2016-09-22 20:38:53 -0700

    Created Function
    Added a function that grabs the value of the search form.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("hello world!");
+// Key 
+// AIzaSyC3V8D2JAlnTsGCKgMi_VHUzgSTjfvWWxQ	
+$(document).ready(function() {
+	$("#search-form").submit(function(e) {
+		e.preventDefault();
+		var search = $("#search").val();
+		console.log(search);
+	});
+});
