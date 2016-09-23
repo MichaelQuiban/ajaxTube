097912585256d4562251d4b4fc623b5f83c9ebe4
Michael Q <MichaelQuiban@outlook.com>
2016-09-22 21:08:06 -0700

    Added URL from the youtube data API.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
 // Key 
 // AIzaSyC3V8D2JAlnTsGCKgMi_VHUzgSTjfvWWxQ	
 $(document).ready(function() {
+	function runSearch(search) {
+		var url = 'https://www.googleapis.com/youtube/v3/search';
+		var params = {
+			part:'snippet',
+			key: 'AIzaSyC3V8D2JAlnTsGCKgMi_VHUzgSTjfvWWxQ',
+			q: search 
+		}
+		$.getJSON(url, params, function(data) {
+			//Function show results/items/array
+			console.log(data);
+		});
+	}
+
 	$("#search-form").submit(function(e) {
 		e.preventDefault();
 		var search = $("#search").val();
-		console.log(search);
+		runSearch(search);
 	});
 });
