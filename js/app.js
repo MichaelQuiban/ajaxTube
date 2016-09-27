ea235ac3d718e34a89e39926246ce1ccbb9d46a4
Michael Q <MichaelQuiban@outlook.com>
2016-09-27 01:04:18 -0700

    Attempted to list API data from Youtube.
    Bug: Shows a ton of results. I only want one specific result from one
    specific object.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-// Key 
-// AIzaSyC3V8D2JAlnTsGCKgMi_VHUzgSTjfvWWxQ	
+// Key: AIzaSyC3V8D2JAlnTsGCKgMi_VHUzgSTjfvWWxQ
+// URL: https://www.googleapis.com/youtube/v3/search
+
 $(document).ready(function() {
 	function runSearch(search) {
 		var url = 'https://www.googleapis.com/youtube/v3/search';
@@ -9,13 +10,26 @@ $(document).ready(function() {
 			q: search 
 		}
 		$.getJSON(url, params, function(data) {
-			//Function show results/items/array
-			console.log(data);
+			var results = data.items;
+			var dataLength = results.length;
+					//Iterate through the results.
+				for ( var i = 0; i < dataLength; i++) {
+					//Show the results so I don't forget.
+					console.log(dataLength);
+					console.log(results);
+					//Append these dang results.
+					$("#results").append("<p> Channel Title:</p>" + results[i].snippet.title + "<br>");
+					$("#results").append("<p> Channel Title:</p>" + results[i].snippet.description + "<br>");
+					$("#results").append("<p> Channel Title:</p>" + results[i].snippet.publishedAt + "<br>");
+					$("#results").append("<p> Channel Title:</p>" + results[i].snippet.channelTitle + "<br>");
+				};
 		});
 	}
 
+//Empty the form and grab the query performed by the user.
 	$("#search-form").submit(function(e) {
 		e.preventDefault();
+		$("#results").empty();
 		var search = $("#search").val();
 		runSearch(search);
 	});
